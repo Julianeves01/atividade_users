de352e9199bd4b6f55004b62346774419cdc938e
Julianeves01 <julianvs2007@gmail.com>
2025-04-15 18:19:16 -0300

    update PostModel.js

--- src/models/PostModel.js
+++ src/models/PostModel.js
@@ -1,13 +1,13 @@
-const { v4: uuid4 } = require('uuid');
-class Post {
-    constructor(idUser, curtidas, comentarios, compartilhamentos, url_imagem) {
-        this.id = uuid4();
-        this.idUser = idUser;
-        this.curtidas = curtidas;
-        this.comentarios = comentarios;
-        this.compartilhamentos = compartilhamentos;
-        this.url_imagem = url_imagem;
+const pool = require("../../config/database");
+
+const getPosts = async () => {
+    if (!title) {
+        const result = await pool.query("SELECT post.*, usuarios.name AS usuario_name
+        FROM post LEFT JOIN usuarios ON post.user_id = usuarios.id");
+        return result.rows
+    }else{
+        
+    }
+
     }
 }
-
-module.exports = Post;
