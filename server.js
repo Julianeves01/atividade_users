832ee36b804908d7b10d5030da8b8e001529ace6
Julianeves01 <julia.s.neves6@aluno.senai.br>
2025-04-17 16:23:35 -0300

    update userController

--- server.js
+++ server.js
@@ -4,20 +4,23 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./src/routes/userRoutes");
 const postRoutes = require("./src/routes/postRoutes");
+const reportRoutes = require("./src/routes/reportRoutes");
+const setupSwagger = require("./src/config/swagger");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
+setupSwagger(app);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/user", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api", reportRoutes);
 
-app.get("/", (req, res) => {
-    res.send("ROTA TA FUNCIONANDO!!");
-});
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(` 🎀 Servidor rodando em http://localhost:${PORT}`);
 });
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/userController");
+const UserController = require("../controllers/userController");
 const upload = require('../config/upload.js');
 
 /**
